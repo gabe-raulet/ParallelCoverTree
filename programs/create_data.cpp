72be004dd1533a839d9b911a0807b38be508eaeb
Gabriel Raulet <gabe.h.raulet@gmail.com>
2024-02-26 18:20:42 -0800

    write data

--- programs/create_data.cpp
+++ programs/create_data.cpp
@@ -13,6 +13,7 @@
 
 double get_neighborhood_graph(CoverTree& ct, double radius, std::vector<std::vector<int64_t>>& nng);
 void write_nng(const std::vector<std::vector<int64_t>>& nng, double radius, const char *output);
+void write_points(const std::vector<Point>& points, const char *output);
 
 int main(int argc, char *argv[])
 {
@@ -47,6 +48,7 @@ int main(int argc, char *argv[])
     output = read_string_arg(argc, argv, "-o", (char**)&output);
 
     std::vector<Point> points = Point::random_points(n, d, seed);
+    write_points(points, output);
     CoverTree ct(points);
     double radius = r0;
     double telapsed;
@@ -90,4 +92,25 @@ void write_nng(const std::vector<std::vector<int64_t>>& nng, double radius, cons
     }
 
     f.close();
+}
+
+void write_points(const std::vector<Point>& points, const char *output)
+{
+    assert(points.size() != 0);
+
+    uint64_t n = points.size();
+    uint64_t d = points.back().getdim();
+    std::string fname = std::string(output) + ".points.bin";
+
+    FILE *f = fopen(fname.c_str(), "wb");
+
+    fwrite(&n, sizeof(uint64_t), 1, f);
+    fwrite(&d, sizeof(uint64_t), 1, f);
+
+    for (uint64_t i = 0; i < n; ++i)
+    {
+        fwrite(points[i].getdata(), sizeof(double), d, f);
+    }
+
+    fclose(f);
 }
