dbd051c455e207425e18b2a6badba4908e125b31
Gabriel Raulet <gabe.h.raulet@gmail.com>
2024-03-11 14:39:58 -0700

    seg fault

--- programs/dist_build_epsilon_graph.cpp
+++ programs/dist_build_epsilon_graph.cpp
@@ -1,6 +1,7 @@
 #include <vector>
 #include <iostream>
 #include <filesystem>
+#include <sstream>
 #include <fstream>
 #include <iterator>
 #include <algorithm>
@@ -61,6 +62,66 @@ int main(int argc, char *argv[])
     if (myrank == 0)
     {
         fprintf(stderr, "Read cover tree from file '%s' :: maxtime = %4f (s), avgtime = %4f (s)\n", infname, maxtime, proctime / nprocs);
+    }
+
+    MPI_Barrier(MPI_COMM_WORLD);
+    elapsed = -MPI_Wtime();
+
+    index_t n = tree.num_points();
+    const float *p = tree.getdata();
+    int d = tree.getdim();
+
+    index_t chunksize = n / nprocs;
+    index_t mychunksize = myrank != nprocs-1? chunksize : n - (nprocs-1)*chunksize;
+    index_t myoffset = myrank * chunksize;
+
+    std::vector<std::vector<index_t>> mygraph;
+    mygraph.resize(mychunksize);
+
+    const float *myp = &p[d*myoffset];
+
+    for (index_t i = 0; i < mychunksize; ++i)
+    {
+        mygraph[i] = tree.radii_query(&myp[d*i], epsilon);
+    }
+
+    elapsed += MPI_Wtime();
+
+    MPI_Reduce(&elapsed, &maxtime,  1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
+    MPI_Reduce(&elapsed, &proctime, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+
+    if (myrank == 0)
+    {
+        fprintf(stderr, "Constructed neighborhood graph :: maxtime = %4f (s), avgtime = %4f (s)\n", maxtime, proctime / nprocs);
+    }
+
+    if (outfname)
+    {
+        std::ostringstream ss;
+        ss << "rank" << myrank << "_" << outfname;
+        std::string fname = ss.str();
+
+        index_t m = 0;
+
+        for (auto it = mygraph.begin(); it != mygraph.end(); ++it)
+        {
+            m += it->size();
+            std::sort(it->begin(), it->end());
+        }
+
+        FILE *f = fopen(fname.c_str(), "w");
+
+        fprintf(f, "%lld\t%lld\t%lld\n", n, n, m);
+
+        for (index_t i = 0; i < n; ++i)
+        {
+            for (index_t j : mygraph[i])
+            {
+                fprintf(f, "%lld\t%lld\n", i+myoffset, j);
+            }
+        }
+
+        fclose(f);
     }
 
     MPI_Finalize();
