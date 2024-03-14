99dea035b1ec10299c3a369c8a4cfda03e4c8f1e
Gabriel Raulet <gabe.h.raulet@gmail.com>
2024-03-14 16:10:26 -0700

    good

--- programs/dist_benchmark_nng.cpp
+++ programs/dist_benchmark_nng.cpp
@@ -1,8 +1,11 @@
 #include <vector>
 #include <algorithm>
 #include <iostream>
+#include <sstream>
+#include <chrono>
 #include <string>
 #include <random>
+#include <iomanip>
 #include <limits.h>
 #include <assert.h>
 #include <stdio.h>
@@ -13,11 +16,24 @@
 #include "CoverTree.h"
 #include "read_args.h"
 
+struct MPITimer
+{
+    MPITimer(MPI_Comm comm, int root);
+    void start_timer();
+    void stop_timer();
+
+    MPI_Comm comm;
+    int root;
+    double t;
+    double maxtime, proctime;
+};
+
 extern double distance(const float *p, const float *q, int d);
 std::vector<float> generate_points(int64_t n, int d, double var, int seed, MPI_Comm comm);
 std::vector<std::vector<int64_t>> build_nng_bf(const std::vector<float>& pointmem, int dim, double radius, int64_t *n_edges, MPI_Comm comm);
 std::vector<std::vector<int64_t>> build_nng_tree(const CoverTree& tree, double radius, int64_t *n_edges, MPI_Comm comm);
 void write_degrees_to_file(const char *fname, std::vector<std::vector<int64_t>>& graph, MPI_Comm comm);
+std::string return_current_time_and_date();
 
 int main(int argc, char *argv[])
 {
@@ -65,79 +81,58 @@ int main(int argc, char *argv[])
         outfname = read_string_arg(argc, argv, "-o", NULL);
     }
 
-    double t, maxtime, proctime;
+    if (myrank == 0)
+    {
+        std::string curtime = return_current_time_and_date();
+        fprintf(stderr, "Running %s at %s\n", argv[0], curtime.c_str());
+    }
 
-    MPI_Barrier(MPI_COMM_WORLD);
-    t = -MPI_Wtime();
+    MPITimer timer(MPI_COMM_WORLD, 0);
 
+    timer.start_timer();
     auto pointmem = generate_points(npoints, dim, var, seed, MPI_COMM_WORLD);
+    timer.stop_timer();
 
-    t += MPI_Wtime();
-    MPI_Reduce(&t, &maxtime,  1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
-    MPI_Reduce(&t, &proctime, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
-
-    if (myrank == 0) fprintf(stderr, "(generate_points) :: [n_points=%lld,dim=%d,var=%.2f] :: [maxtime=%.4f,avgtime=%.4f (seconds)]\n", npoints, dim, var, maxtime, proctime/nprocs);
+    if (myrank == timer.root) fprintf(stderr, "(generate_points) :: [n_points=%lld,dim=%d,var=%.2f] :: [maxtime=%.4f,avgtime=%.4f (seconds)]\n", npoints, dim, var, timer.maxtime, timer.proctime/nprocs);
 
     int64_t m, n_edges;
     std::vector<std::vector<int64_t>> local_dist_graph;
 
     if (bf)
     {
-        MPI_Barrier(MPI_COMM_WORLD);
-        t = -MPI_Wtime();
-
+        timer.start_timer();
         local_dist_graph = build_nng_bf(pointmem, dim, radius, &m, MPI_COMM_WORLD);
-
         MPI_Reduce(&m, &n_edges, 1, MPI_INT64_T, MPI_SUM, 0, MPI_COMM_WORLD);
+        timer.stop_timer();
 
-        t += MPI_Wtime();
-        MPI_Reduce(&t, &maxtime,  1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
-        MPI_Reduce(&t, &proctime, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
-
-        if (myrank == 0) fprintf(stderr, "(build_nng_bf) :: [n_verts=%lld,n_edges=%lld,avg_deg=%.2f,radius=%.2f] :: [maxtime=%.4f,avgtime=%.4f (seconds)]\n", npoints, n_edges, (n_edges+0.0)/npoints, radius, maxtime, proctime/nprocs);
+        if (myrank == timer.root) fprintf(stderr, "(build_nng_bf) :: [n_verts=%lld,n_edges=%lld,avg_deg=%.2f,radius=%.2f] :: [maxtime=%.4f,avgtime=%.4f (seconds)]\n", npoints, n_edges, (n_edges+0.0)/npoints, radius, timer.maxtime, timer.proctime/nprocs);
     }
     else
     {
-        MPI_Barrier(MPI_COMM_WORLD);
-        t = -MPI_Wtime();
-
         const float *p = pointmem.data();
         double base = 2.;
 
+        timer.start_timer();
         CoverTree tree(p, npoints, dim, base);
+        timer.stop_timer();
 
-        t += MPI_Wtime();
-        MPI_Reduce(&t, &maxtime,  1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
-        MPI_Reduce(&t, &proctime, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+        if (myrank == timer.root) fprintf(stderr, "(build_cover_tree) :: [n_verts=%lld,base=%.2f] :: [maxtime=%.4f,avgtime=%.4f (seconds)]\n", tree.num_vertices(), base, timer.maxtime, timer.proctime/nprocs);
 
-        if (myrank == 0) fprintf(stderr, "(build_cover_tree) :: [n_verts=%lld,base=%.2f] :: [maxtime=%.4f,avgtime=%.4f (seconds)]\n", tree.num_vertices(), base, maxtime, proctime/nprocs);
-
-        MPI_Barrier(MPI_COMM_WORLD);
-        t = -MPI_Wtime();
-
+        timer.start_timer();
         local_dist_graph = build_nng_tree(tree, radius, &m, MPI_COMM_WORLD);
-
         MPI_Reduce(&m, &n_edges, 1, MPI_INT64_T, MPI_SUM, 0, MPI_COMM_WORLD);
+        timer.stop_timer();
 
-        t += MPI_Wtime();
-        MPI_Reduce(&t, &maxtime,  1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
-        MPI_Reduce(&t, &proctime, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
-
-        if (myrank == 0) fprintf(stderr, "(build_nng_tree) :: [n_verts=%lld,n_edges=%lld,avg_deg=%.2f,radius=%.2f] :: [maxtime=%.4f,avgtime=%.4f (seconds)]\n", npoints, n_edges, (n_edges+0.0)/npoints, radius, maxtime, proctime/nprocs);
+        if (myrank == timer.root) fprintf(stderr, "(build_nng_tree) :: [n_verts=%lld,n_edges=%lld,avg_deg=%.2f,radius=%.2f] :: [maxtime=%.4f,avgtime=%.4f (seconds)]\n", npoints, n_edges, (n_edges+0.0)/npoints, radius, timer.maxtime, timer.proctime/nprocs);
     }
 
     if (outfname)
     {
-        MPI_Barrier(MPI_COMM_WORLD);
-        t = -MPI_Wtime();
-
+        timer.start_timer();
         write_degrees_to_file(outfname, local_dist_graph, MPI_COMM_WORLD);
+        timer.stop_timer();
 
-        t += MPI_Wtime();
-        MPI_Reduce(&t, &maxtime,  1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
-        MPI_Reduce(&t, &proctime, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
-
-        if (myrank == 0) fprintf(stderr, "(write_degrees_to_file) :: [outfname='%s'] :: [maxtime=%.4f,avgtime=%.4f (seconds)]\n", outfname, maxtime, proctime/nprocs);
+        if (myrank == timer.root) fprintf(stderr, "(write_degrees_to_file) :: [outfname='%s'] :: [maxtime=%.4f,avgtime=%.4f (seconds)]\n", outfname, timer.maxtime, timer.proctime/nprocs);
     }
 
     MPI_Finalize();
@@ -261,4 +256,33 @@ void write_degrees_to_file(const char *fname, std::vector<std::vector<int64_t>>&
         for (auto d : degrees) fprintf(f, "%lld\n", d);
         fclose(f);
     }
+}
+
+std::string return_current_time_and_date()
+{
+    /*
+     * Shamelessly copied from here: https://stackoverflow.com/questions/17223096/outputting-date-and-time-in-c-using-stdchrono
+     */
+
+    auto now = std::chrono::system_clock::now();
+    auto in_time_t = std::chrono::system_clock::to_time_t(now);
+
+    std::stringstream ss;
+    ss << std::put_time(std::localtime(&in_time_t), "%Y-%m-%d %X");
+    return ss.str();
+}
+
+MPITimer::MPITimer(MPI_Comm comm, int root) : comm(comm), root(root) {}
+
+void MPITimer::start_timer()
+{
+    MPI_Barrier(comm);
+    t = -MPI_Wtime();
+}
+
+void MPITimer::stop_timer()
+{
+    t += MPI_Wtime();
+    MPI_Reduce(&t, &maxtime,  1, MPI_DOUBLE, MPI_MAX, root, comm);
+    MPI_Reduce(&t, &proctime, 1, MPI_DOUBLE, MPI_SUM, root, comm);
 }
