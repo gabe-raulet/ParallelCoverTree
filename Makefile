9bde9ae78a243edd350a4869a0e17ae4ea9acaa2
Gabriel Raulet <gabe.h.raulet@gmail.com>
2024-03-14 13:36:41 -0700

    benchmarking work

--- Makefile
+++ Makefile
@@ -20,48 +20,16 @@ endif
 
 INCLUDES=-I./include
 
-all: build_tree build_nng build_nng_bf benchmark_nng benchmark_nng_bf create_data
-
-install: cluster_test build_tree build_nng build_nng_bf benchmark_nng create_data
-	cp cluster_test /global/homes/g/gabeh98/software/cover_tree
-	cp build_tree /global/homes/g/gabeh98/software/cover_tree
-	cp build_nng /global/homes/g/gabeh98/software/cover_tree
-	cp build_nng_bf /global/homes/g/gabeh98/software/cover_tree
-	cp benchmark_nng /global/homes/g/gabeh98/software/cover_tree
-	cp create_data /global/homes/g/gabeh98/software/cover_tree
-
-test: cluster_test build_tree
-	@./build_tree -i testdata/points.1K.2d.fvecs -o testdata/1K.2d.cover_tree
-	@./cluster_test -i testdata/1K.2d.cover_tree -r 0.075 -o testdata/points.1K.2d.r075.clusters.compare
-	@diff -s testdata/points.1K.2d.r075.clusters testdata/points.1K.2d.r075.clusters.compare
-	@./build_tree -i testdata/points.1K.4d.fvecs -o testdata/1K.4d.cover_tree
-	@./cluster_test -i testdata/1K.4d.cover_tree -r 0.3 -o testdata/points.1K.4d.r3.clusters.compare
-	@diff -s testdata/points.1K.4d.r3.clusters testdata/points.1K.4d.r3.clusters.compare
-	@./build_tree -i testdata/points.1K.8d.fvecs -o testdata/1K.8d.cover_tree
-	@./cluster_test -i testdata/1K.8d.cover_tree -r 0.975 -o testdata/points.1K.8d.r975.clusters.compare
-	@diff -s testdata/points.1K.8d.r975.clusters testdata/points.1K.8d.r975.clusters.compare
-	@rm -rf testdata/*.clusters.compare testdata/*.cover_tree
-
-cluster_test: programs/cluster_test.cpp src/CoverTree.cpp include/CoverTree.h src/VectorIO.cpp include/VectorIO.h src/read_args.cpp include/read_args.h
-	$(CXX) -o cluster_test $(INCLUDES) $(FLAGS) programs/cluster_test.cpp src/CoverTree.cpp src/VectorIO.cpp src/read_args.cpp
-
-build_tree: programs/build_tree.cpp src/CoverTree.cpp include/CoverTree.h src/VectorIO.cpp include/VectorIO.h src/read_args.cpp include/read_args.h
-	$(CXX) -o build_tree $(INCLUDES) $(FLAGS) programs/build_tree.cpp src/CoverTree.cpp src/VectorIO.cpp src/read_args.cpp
-
-build_nng: programs/build_nng.cpp src/CoverTree.cpp include/CoverTree.h src/VectorIO.cpp include/VectorIO.h src/read_args.cpp include/read_args.h
-	$(CXX) -o build_nng $(INCLUDES) $(FLAGS) programs/build_nng.cpp src/CoverTree.cpp src/VectorIO.cpp src/read_args.cpp
-
-build_nng_bf: programs/build_nng_bf.cpp src/VectorIO.cpp include/VectorIO.h src/read_args.cpp include/read_args.h
-	$(CXX) -o build_nng_bf $(INCLUDES) $(FLAGS) programs/build_nng_bf.cpp src/VectorIO.cpp src/read_args.cpp
+all: benchmark_nng benchmark_nng_bf dist_benchmark_nng_bf
 
 benchmark_nng: programs/benchmark_nng.cpp src/CoverTree.cpp include/CoverTree.h src/VectorIO.cpp include/VectorIO.h src/read_args.cpp include/read_args.h
 	$(CXX) -o benchmark_nng $(INCLUDES) $(FLAGS) programs/benchmark_nng.cpp src/CoverTree.cpp src/VectorIO.cpp src/read_args.cpp
 
-benchmark_nng_bf: programs/benchmark_nng_bf.cpp src/VectorIO.cpp include/VectorIO.h src/read_args.cpp include/read_args.h
-	$(CXX) -o benchmark_nng_bf $(INCLUDES) $(FLAGS) programs/benchmark_nng_bf.cpp src/VectorIO.cpp src/read_args.cpp
+benchmark_nng_bf: programs/benchmark_nng_bf.cpp src/read_args.cpp include/read_args.h
+	$(CXX) -o benchmark_nng_bf $(INCLUDES) $(FLAGS) programs/benchmark_nng_bf.cpp src/read_args.cpp
 
-create_data: programs/create_data.cpp src/VectorIO.cpp include/VectorIO.h src/read_args.cpp include/read_args.h
-	$(CXX) -o create_data $(INCLUDES) $(FLAGS) programs/create_data.cpp src/VectorIO.cpp src/read_args.cpp
+dist_benchmark_nng_bf: programs/dist_benchmark_nng_bf.cpp src/read_args.cpp include/read_args.h
+	$(MPICXX) -o dist_benchmark_nng_bf $(INCLUDES) $(MPIFLAGS) programs/dist_benchmark_nng_bf.cpp src/read_args.cpp
 
 clean:
-	rm -rf *.dSYM *.bin *.fvecs cluster_test build_tree build_nng build_nng_bf benchmark_nng benchmark_nng_bf create_data
+	rm -rf *.dSYM *.bin *.fvecs benchmark_nng benchmark_nng_bf dist_benchmark_nng_bf
---
+++ programs/dist_benchmark_nng_bf.cpp
@@ -0,0 +1,157 @@
+#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <random>
+#include <limits.h>
+#include <assert.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+#include <math.h>
+#include <mpi.h>
+#include "read_args.h"
+
+double distance(const float *p, const float *q, int d);
+std::vector<float> generate_points(int64_t n, int d, double var, int seed, MPI_Comm comm);
+std::vector<std::vector<int64_t>> build_nng_bf(const std::vector<float>& pointmem, int dim, double radius, int64_t *n_edges, MPI_Comm comm);
+
+int main(int argc, char *argv[])
+{
+    int64_t npoints;
+    int dim;
+    double radius;
+    double var;
+    int seed = -1;
+    char *outfname = NULL;
+
+    int myrank, nprocs;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
+    MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
+
+    if (argc == 1 || find_arg_idx(argc, argv, "-h") >= 0)
+    {
+        if (myrank == 0)
+        {
+            fprintf(stderr, "Usage: %s [..]\n", argv[0]);
+            fprintf(stderr, "Parameters: -n INT    number of points\n");
+            fprintf(stderr, "            -d INT    dimension\n");
+            fprintf(stderr, "            -V FLOAT  variance\n");
+            fprintf(stderr, "            -r FLOAT  epsilon radius\n");
+            fprintf(stderr, "            -o FILE   graph degrees file [optional]\n");
+            fprintf(stderr, "            -s INT    rng seed [default: random]\n");
+            fprintf(stderr, "            -h        help message\n");
+        }
+
+        MPI_Finalize();
+        return 0;
+    }
+
+    npoints = read_formatted_int_arg(argc, argv, "-n", NULL);
+    dim = read_int_arg(argc, argv, "-d", NULL);
+    radius = read_double_arg(argc, argv, "-r", NULL);
+    var = read_double_arg(argc, argv, "-V", NULL);
+    seed = read_int_arg(argc, argv, "-s", &seed);
+
+    if (find_arg_idx(argc, argv, "-o") >= 0)
+    {
+        outfname = read_string_arg(argc, argv, "-o", NULL);
+    }
+
+    double t, maxtime, proctime;
+
+    MPI_Barrier(MPI_COMM_WORLD);
+    t = -MPI_Wtime();
+
+    auto pointmem = generate_points(npoints, dim, var, seed, MPI_COMM_WORLD);
+
+    t += MPI_Wtime();
+    MPI_Reduce(&t, &maxtime,  1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
+    MPI_Reduce(&t, &proctime, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+
+    if (myrank == 0) fprintf(stderr, "(generate_points) :: [n_points=%lld,dim=%d,var=%.2f] :: [maxtime=%.4f,avgtime=%.4f (seconds)]\n", npoints, dim, var, maxtime, proctime/nprocs);
+
+    int64_t m, n_edges;
+
+    MPI_Barrier(MPI_COMM_WORLD);
+    t = -MPI_Wtime();
+
+    auto local_dist_graph = build_nng_bf(pointmem, dim, radius, &m, MPI_COMM_WORLD);
+
+    MPI_Reduce(&m, &n_edges, 1, MPI_INT64_T, MPI_SUM, 0, MPI_COMM_WORLD);
+
+    t += MPI_Wtime();
+    MPI_Reduce(&t, &maxtime,  1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
+    MPI_Reduce(&t, &proctime, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+
+    if (myrank == 0) fprintf(stderr, "(build_nng_bf) :: [n_verts=%lld,n_edges=%lld,avg_deg=%.2f,radius=%.2f] :: [maxtime=%.4f,avgtime=%.4f (seconds)]\n", npoints, n_edges, (n_edges+0.0)/npoints, radius, maxtime, proctime/nprocs);
+
+    MPI_Finalize();
+    return 0;
+}
+
+std::vector<float> generate_points(int64_t n, int d, double var, int seed, MPI_Comm comm)
+{
+    int myrank, nprocs;
+    MPI_Comm_rank(comm, &myrank);
+    MPI_Comm_size(comm, &nprocs);
+
+    std::vector<float> pointmem(d*n);
+
+    if (myrank == 0)
+    {
+        std::random_device rd;
+        std::default_random_engine gen(seed < 0? rd() : seed*17);
+        std::normal_distribution dis{0.0, std::sqrt(var)};
+        std::generate(pointmem.begin(), pointmem.end(), [&]() { return dis(gen); });
+    }
+
+    MPI_Bcast(pointmem.data(), static_cast<int>(n*d), MPI_FLOAT, 0, comm);
+    return std::move(pointmem);
+}
+
+std::vector<std::vector<int64_t>> build_nng_bf(const std::vector<float>& pointmem, int dim, double radius, int64_t *n_edges, MPI_Comm comm)
+{
+    int myrank, nprocs;
+    MPI_Comm_rank(comm, &myrank);
+    MPI_Comm_size(comm, &nprocs);
+
+    int64_t n = pointmem.size() / dim;
+    const float *pdata = pointmem.data();
+
+    int64_t chunksize = n / nprocs;
+    int64_t start = myrank * chunksize;
+    int64_t stop = std::min(start + chunksize, n);
+    int64_t mychunksize = stop - start;
+
+    std::vector<std::vector<int64_t>> graph(mychunksize);
+
+    int64_t m = 0;
+
+    for (int64_t u = start; u < stop; ++u)
+    {
+        for (int64_t v = 0; v < n; ++v)
+            if (distance(&pdata[dim*u], &pdata[dim*v], dim) <= radius)
+                graph[u-start].push_back(v);
+
+        m += graph[u-start].size();
+    }
+
+    *n_edges = m;
+
+    return std::move(graph);
+}
+
+double distance(const float *p, const float *q, int d)
+{
+    double sum = 0.0, di;
+
+    for (int i = 0; i < d; ++i)
+    {
+        di = p[i] - q[i];
+        sum += di*di;
+    }
+
+    return std::sqrt(sum);
+}
