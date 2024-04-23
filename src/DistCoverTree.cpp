9121420afb81a23592542944de887406e7445e89
Gabriel Raulet <gabe.h.raulet@gmail.com>
2024-04-23 10:53:36 -0700

    distribute compute farthest hub points

--- src/DistCoverTree.cpp
+++ src/DistCoverTree.cpp
@@ -74,7 +74,91 @@ void DistCoverTree::initialize_root_hub()
     MPI_Allreduce(MPI_IN_PLACE, &max_radius, 1, MPI_DOUBLE, MPI_MAX, comm);
 }
 
+struct ArgmaxPair
+{
+    int64_t index;
+    double value;
+
+    ArgmaxPair(int64_t index, double value) : index(index), value(value) {}
+
+    static void create_mpi_dtype(MPI_Datatype *dtype)
+    {
+        int blklens[2] = {1,1};
+        MPI_Aint disps[2] = {offsetof(ArgmaxPair, index), offsetof(ArgmaxPair, value)};
+        MPI_Datatype types[2] = {MPI_INT64_T, MPI_DOUBLE};
+        MPI_Type_create_struct(2, blklens, disps, types, dtype);
+        MPI_Type_commit(dtype);
+    }
+
+    static void mpi_argmax(void *_in, void *_inout, int *len, MPI_Datatype *dtype)
+    {
+        ArgmaxPair *in = (ArgmaxPair*)_in;
+        ArgmaxPair *inout = (ArgmaxPair*)_inout;
+
+        for (int i = 0; i < *len; ++i)
+            if (inout[i].value < in[i].value)
+            {
+                inout[i].value = in[i].value;
+                inout[i].index = in[i].index;
+            }
+    }
+
+    static void create_mpi_op(MPI_Op *op)
+    {
+        MPI_Op_create(&mpi_argmax, 1, op);
+    }
+};
+
+
 void DistCoverTree::compute_farthest_hub_pts()
 {
-    return;
+    unordered_map<int64_t, pair<int64_t, double>> my_argmaxes; // maps hub id to (point id, distance) pair
+    transform(hub_chains.begin(), hub_chains.end(), inserter(my_argmaxes, my_argmaxes.end()),
+            [](const auto& chain) { return make_pair(chain.first, make_pair(-1, -1.0)); });
+
+    for (int64_t i = 0; i < mysize; ++i)
+    {
+        int64_t hub_id = my_hub_vtx_ids[i];
+
+        if (hub_id >= 0)
+        {
+            auto& it = my_argmaxes.find(hub_id)->second;
+
+            if (my_dists[i] > it.second)
+            {
+                it.first = i;
+                it.second = my_dists[i];
+            }
+        }
+    }
+
+    vector<int64_t> hub_ids;
+    vector<ArgmaxPair> my_argmax_pairs;
+
+    hub_ids.reserve(my_argmaxes.size());
+    my_argmax_pairs.reserve(my_argmaxes.size());
+
+    for (auto it = my_argmaxes.begin(); it != my_argmaxes.end(); ++it)
+    {
+        hub_ids.push_back(it->first);
+        my_argmax_pairs.emplace_back(it->second.first + myoffset, it->second.second);
+    }
+
+    MPI_Op MPI_ARGMAX;
+    MPI_Datatype MPI_ARGMAX_PAIR;
+
+    ArgmaxPair::create_mpi_op(&MPI_ARGMAX);
+    ArgmaxPair::create_mpi_dtype(&MPI_ARGMAX_PAIR);
+
+    MPI_Allreduce(MPI_IN_PLACE, my_argmax_pairs.data(), static_cast<int>(my_argmax_pairs.size()), MPI_ARGMAX_PAIR, MPI_ARGMAX, comm);
+
+    MPI_Op_free(&MPI_ARGMAX);
+    MPI_Type_free(&MPI_ARGMAX_PAIR);
+
+    farthest_hub_pts.clear();
+
+    for (int64_t i = 0; i < my_argmax_pairs.size(); ++i)
+    {
+        farthest_hub_pts.insert({hub_ids[i], my_argmax_pairs[i].index});
+    }
 }
